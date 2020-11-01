8f914416fd9d02a9d9cb65a7250ad9c395178a58
fourat <fouratmahmoudi@gmail.Com>
2020-11-01 15:59:34 +0100

    Second Commit all interfaces Added

--- app/src/main/java/com/hightech/miniweatherstation/dashboard.java
+++ app/src/main/java/com/hightech/miniweatherstation/dashboard.java
@@ -10,6 +10,15 @@ import android.widget.Button;
 public class dashboard extends AppCompatActivity {
 
     private Button summary;
+    private Button dust;
+    private Button temp;
+    private Button hum;
+    private Button light;
+    private Button press;
+    private Button uv;
+
+
+
 
 
     @Override
@@ -23,11 +32,76 @@ public class dashboard extends AppCompatActivity {
                 opensummary();
             }
         });
-
+        dust = (Button) findViewById(R.id.du);
+        dust.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                opendust();
+            }
+        });
+        temp = (Button) findViewById(R.id.temp);
+        temp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                opentemp();
+            }
+        });
+        hum = (Button) findViewById(R.id.humidity);
+        hum.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openhum();
+            }
+        });
+        light = (Button) findViewById(R.id.light);
+        light.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openlight();
+            }
+        });
+        press = (Button) findViewById(R.id.pression);
+        press.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openpress();
+            }
+        });
+        uv = (Button) findViewById(R.id.uv);
+        uv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openuv();
+            }
+        });
     }
 
     public void opensummary () {
         Intent intent = new Intent(this, com.hightech.miniweatherstation.summary.class);
+        startActivity(intent);
+    }
+    public void opendust () {
+        Intent intent = new Intent(this, com.hightech.miniweatherstation.dust.class);
+        startActivity(intent);
+    }
+    public void opentemp () {
+        Intent intent = new Intent(this, com.hightech.miniweatherstation.temp.class);
+        startActivity(intent);
+    }
+    public void openhum () {
+        Intent intent = new Intent(this, com.hightech.miniweatherstation.humidity.class);
+        startActivity(intent);
+    }
+    public void openlight () {
+        Intent intent = new Intent(this, com.hightech.miniweatherstation.light.class);
+        startActivity(intent);
+    }
+    public void openpress () {
+        Intent intent = new Intent(this, com.hightech.miniweatherstation.pression.class);
+        startActivity(intent);
+    }
+    public void openuv () {
+        Intent intent = new Intent(this, com.hightech.miniweatherstation.Uv.class);
         startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/hightech/miniweatherstation/temp.java
@@ -0,0 +1,33 @@
+package com.hightech.miniweatherstation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.Date;
+
+public class temp extends AppCompatActivity {
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_temp);
+
+
+
+    }
+
+
+
+
+
+
+
+}
